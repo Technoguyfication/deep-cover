b4fa2593e6706feb3703e22ccb429f3df2aa49df
Hayden Andreyka <haydenandreyka@gmail.com>
2021-05-28 01:03:55 -0700

    create project

---
+++ deepcover/src/main/java/com/technoguyfication/deepcover/DeepCover.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.technoguyfication.deepcover;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Hayden
+ */
+public class DeepCover extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+    
+}
